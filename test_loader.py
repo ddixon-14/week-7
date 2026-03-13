54ea0954cc6fb098df66d576de7e9944d905d889
Danielle D. <dani.dixonpro@gmail.com>
2026-03-13 19:17:44 -0400

    addition of both locations from exercise 2

--- test_loader.py
+++ test_loader.py
@@ -10,17 +10,30 @@ class TestLoader(unittest.TestCase):
         fake_location.longitude = -73.9782438
         fake_location.geo_type = "Museum"
 
+        fake_location2 = Mock()
+        fake_location2.latitude = 30.684373
+        fake_location2.longitude = -88.015316
+        fake_location2.geo_type = "Park"
+
         #fake geolocator
         fake_geo = Mock()
-        fake_geo.geocode.return_value = fake_location
+        fake_geo.geocode.side_effect = [fake_location, fake_location2]
 
         #function call 
         result = fetch_location_data(fake_geo, "MoMa")
+        result2 = fetch_location_data(fake_geo, "USS Alabama Battleship Park")
 
         self.assertEqual(result["location"], "MoMa")
         self.assertEqual(result["latitude"], 40.7618552)
         self.assertEqual(result["longitude"], -73.9782438)
         self.assertEqual(result["type"], "Museum")
+
+        self.assertEqual(result2["location"], "USS Alabama Battleship Park")
+        self.assertEqual(result2["latitude"], 30.684373)
+        self.assertEqual(result2["longitude"], -88.015316)
+        self.assertEqual(result2["type"], "Park")
+
+        
 
     def test_invalid_location(self):
         geolocator = get_geolocator()
