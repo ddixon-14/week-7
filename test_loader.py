e1c6a6d438589d8fabc5e8138f594e6d81583be5
Danielle D. <dani.dixonpro@gmail.com>
2026-03-13 17:44:27 -0400

    unittests passed for nonetype and valid locations

--- test_loader.py
+++ test_loader.py
@@ -1,11 +1,26 @@
 import unittest
+from unittest.mock import Mock
 import pandas as pd
 from loader import *
-
 class TestLoader(unittest.TestCase):
     def test_valid_locations(self):
-        # your code here
-        return None
+        #creates a fake location object
+        fake_location = Mock()
+        fake_location.latitude = 40.4
+        fake_location.longitude = 79.9
+        fake_location.geo_type = "city"
+
+        #fake geolocator
+        fake_geo = Mock()
+        fake_geo.geocode.return_value = fake_location
+
+        #function call 
+        result = fetch_location_data(fake_geo, "Indiana")
+
+        self.assertEqual(result["location"], "Indiana")
+        self.assertEqual(result["latitude"], 40.4)
+        self.assertEqual(result["longitude"], 79.9)
+        self.assertEqual(result["type"], "city")
 
     def test_invalid_location(self):
         geolocator = get_geolocator()
